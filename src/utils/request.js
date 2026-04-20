70d7ead1098b47b7aac148e831622c21f1b6e694
张同学 <zjun2002@outlook.com>
2026-04-20 21:28:08 +0800

    增加反向代理配置

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 // 开发环境：本地Flask地址 | 生产环境：运行npm run build的时候自动替换为线上接口地址
 const baseURL = process.env.NODE_ENV === 'development'
-  ? 'http://localhost:8086'  // 开发环境-本地地址
-  : 'http://192.168.1.86:8086' // 生产环境-线上地址（替换成你的真实地址）
+  ? 'https://859707243.xyz:21352'  // 开发环境-本地地址
+  : 'https://192.168.1.86:21352' // 生产环境-线上地址（替换成你的真实地址）
 
 
 // 1. 创建axios实例（只做基础配置，不在headers中写死Token）
